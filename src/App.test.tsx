21529cabc0d74a86428ea154d0ab78ac7c15dd52
Wojciech Olejarz <olejarz.ikt.tjenester@gmail.com>
2023-05-19 16:56:27 +0200

    WIP

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import server from "./mocks/server";
 
@@ -6,12 +6,16 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test("Renders two stations with no available locks and two stations with no available bicycles", () => {
+test("Renders table name and header", async () => {
   render(<App />);
   //Static content test
   const linkElement = screen.getByText(/OSLOBYSYKKEL/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("Renders two stations with no available locks and two stations with no available bicycles", async () => {
+  render(<App />);
   //Dynamic content test
-  // const station1 = screen.getByRole("heading");
-  // expect(station1).toHaveTextContent("Aker Brygge 3 mot Fergene");
+  const station1 = screen.getByRole("heading");
+  expect(station1).toHaveTextContent("Aker Brygge 3 mot Fergene");
 });
